66358f26ac62932069f8228cf4e6ad8c0fd9108e
H9inger <gapper15@gmail.com>
2022-03-12 19:59:33 -0500

    Created layout
    Started with the default via map and edited to personal preference.

--- keyboards/mechwild/murphpad/keymaps/H9inger/keymap.c
+++ keyboards/mechwild/murphpad/keymaps/H9inger/keymap.c
@@ -20,31 +20,31 @@ enum layer_names {
   _BASE,
   _FN1,
 	_FN2,
-	_FN3
+	_RGB
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* Base */
     [_BASE] = LAYOUT(
-                  KC_F1,   KC_F2,   KC_F3,   KC_F4,
-				          KC_NLCK, KC_PSLS, KC_PAST, KC_PMNS,
-                  KC_P7,   KC_P8,   KC_P9,   KC_PPLS,
-        KC_MUTE,  KC_P4,   KC_P5,   KC_P6,   _______,
-        MO(_FN1), KC_P1,   KC_P2,   KC_P3,   KC_PENT,
-        KC_BSPC,  KC_P0,   _______, KC_PDOT, _______,
+                  KC_F1,   KC_F2,   KC_F3,   KC_MPLY,
+				          KC_CALC, KC_PSLS, KC_PAST, KC_PMNS,
+                  KC_7,   KC_8,   KC_9,   KC_PLUS,
+        KC_MUTE,  KC_4,   KC_5,   KC_6,   _______,
+        KC_BTN3, KC_1,   KC_2,   KC_3,   LT(_FN1,KC_ENT),
+        KC_RCTL,  KC_P0,   _______, KC_PDOT, _______,
 
 				          KC_F5,   KC_F6,   KC_F7
 
     ),
     [_FN1] = LAYOUT(
-                 _______,  _______, _______, _______,
-                 _______,  _______, _______, _______,
-                 RGB_HUD,  RGB_SPI, RGB_HUI, _______,
-        _______, RGB_RMOD, RGB_TOG, RGB_MOD, _______,
-        _______, RGB_VAD,  RGB_SPD, RGB_VAI, _______,
-        _______, RGB_SAD,  _______, RGB_SAI, _______,
+                 C(KC_A), C(KC_C), C(KC_V), _______,
+                  KC_BSPC, XXXXXXX, S(KC_SCLN), KC_DEL,
+                  KC_HOME, KC_UP, KC_END, XXXXXXX,
+        _______,  KC_LEFT, _______, KC_RGHT, _______,
+        _______,  KC_INS, KC_DOWN, KC_COMM, _______,
+        _______,  KC_SPC, _______, KC_LSFT, _______,
 
-                 _______,  _______, _______
+                  _______, _______, _______
 
     ),
     [_FN2] = LAYOUT(
@@ -58,15 +58,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                   _______, _______, _______
 
     ),
-	  [_FN3] = LAYOUT(
-                  _______, _______, _______, _______,
-                  _______, _______, _______, _______,
-                  _______, _______, _______, _______,
-        _______,  _______, _______, _______, _______,
-        _______,  _______, _______, _______, _______,
-        _______,  _______, _______, _______, _______,
+	  [_RGB] = LAYOUT(
+                  _______,  _______, _______, _______,
+                 _______,  _______, _______, _______,
+                 RGB_HUD,  RGB_SPI, RGB_HUI, _______,
+        _______, RGB_RMOD, RGB_TOG, RGB_MOD, _______,
+        _______, RGB_VAD,  RGB_SPD, RGB_VAI, _______,
+        _______, RGB_SAD,  _______, RGB_SAI, _______,
 
-                  _______, _______, _______
+                 _______,  _______, _______
 
     )
 };
@@ -76,16 +76,16 @@ bool encoder_update_user(uint8_t index, bool clockwise) {
     switch (index) {
         case 0:
             if (clockwise) {
-                tap_code(KC_VOLU);
+                tap_code(KC_WH_D);
             } else {
-                tap_code(KC_VOLD);
+                tap_code(KC_WH_U);
             }
         break;
 		case 1:
             if (clockwise) {
-                tap_code(KC_BRIU);
+                tap_code(KC_VOLU);
             } else {
-                tap_code(KC_BRID);
+                tap_code(KC_VOLD);
             }
         break;
     }
@@ -98,23 +98,7 @@ bool encoder_update_user(uint8_t index, bool clockwise) {
 		return OLED_ROTATION_270;       // flips the display 270 degrees
 	}
 
-	static void render_logo(void) {     // Render MechWild "MW" Logo
-		static const char PROGMEM logo_1[] = {0x8A, 0x8B, 0x8C, 0x8D, 0x00};
-		static const char PROGMEM logo_2[] = {0xAA, 0xAB, 0xAC, 0xAD, 0xAE, 0x00};
-		static const char PROGMEM logo_3[] = {0xCA, 0xCB, 0xCC, 0xCD, 0x00};
-		static const char PROGMEM logo_4[] = {0x20, 0x8E, 0x8F, 0x90, 0x00};
-		oled_set_cursor(0,0);
-		oled_write_P(logo_1, false);
-		oled_set_cursor(0,1);
-		oled_write_P(logo_2, false);
-		oled_set_cursor(0,2);
-		oled_write_P(logo_3, false);
-		oled_set_cursor(0,3);
-		oled_write_P(logo_4, false);
-	}
-
 	bool oled_task_user(void) {
-		render_logo();
 		oled_set_cursor(0,6);
 
 		oled_write_ln_P(PSTR("Layer"), false);
@@ -124,13 +108,13 @@ bool encoder_update_user(uint8_t index, bool clockwise) {
             oled_write_ln_P(PSTR("Base"), false);
             break;
         case _FN1:
-            oled_write_ln_P(PSTR("FN 1"), false);
+            oled_write_ln_P(PSTR("EDITS"), false);
             break;
         case _FN2:
             oled_write_ln_P(PSTR("FN 2"), false);
             break;
-        case _FN3:
-            oled_write_ln_P(PSTR("FN 3"), false);
+        case _RGB:
+            oled_write_ln_P(PSTR("RGB"), false);
             break;
         default:
             oled_write_ln_P(PSTR("Undef"), false);
