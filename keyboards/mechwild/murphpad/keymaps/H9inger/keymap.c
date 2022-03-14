187c1ea8ae830b82b307688e6b701a4aafaa9b3b
H9inger <85914546+H9inger@users.noreply.github.com>
2022-03-14 08:26:20 -0400

    OLED Update
    Changed readout for host keyboard LED status'

--- keyboards/mechwild/murphpad/keymaps/H9inger/keymap.c
+++ keyboards/mechwild/murphpad/keymaps/H9inger/keymap.c
@@ -137,8 +137,8 @@ bool encoder_update_user(uint8_t index, bool clockwise) {
 	oled_write_ln_P(PSTR(""), false);
     // Host Keyboard LED Status
     led_t led_state = host_keyboard_led_state();
-    oled_write_ln_P(led_state.num_lock ? PSTR("NUM ") : PSTR("    "), false);
-    oled_write_ln_P(led_state.caps_lock ? PSTR("CAP ") : PSTR("    "), false);
+    oled_write_ln_P(led_state.num_lock ? PSTR("NUM_LK ") : PSTR("    "), false);
+    oled_write_ln_P(led_state.caps_lock ? PSTR("CAPS ") : PSTR("    "), false);
     oled_write_ln_P(led_state.scroll_lock ? PSTR("SCR ") : PSTR("    "), false);
     return false;
 	}
